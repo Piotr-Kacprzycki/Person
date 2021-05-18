b5713d7465d71c4a7553caafaa3fd1dd4a9f22f0
Piotr kacprzycki <piotrek634@wp.pl>
2021-05-18 21:33:17 +0200

    add main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const logoJs = document.querySelector('#logoJs');
+const body = document.querySelector('body');
+const colors = ['red','blue','white','green','yellow']
+
+
+
+
+logoJs.addEventListener('click',()=>{
+
+
+
+console.log('dziala')
+
+
+
+});
