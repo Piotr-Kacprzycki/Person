c640677509db3af7ffdeb808ef4afe30e07d544b
Piotr kacprzycki <piotrek634@wp.pl>
2021-05-19 19:41:09 +0200

    add script js,img effect shadow

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
+
 const logoJs = document.querySelector('#logoJs');
 const body = document.querySelector('body');
-const colors = ['red','blue','white','green','yellow']
+const colors = ['red','blue','green','white','yellow','orange'];
+
+ let shadowNumber = Math.floor(Math.random()* colors.length)
+body.addEventListener('mousemove',()=>{
+
+let number = 0;
+
+
+
+setInterval(() => {
+    if(number >= 0){
 
 
 
 
-logoJs.addEventListener('click',()=>{
+    
+number += shadowNumber
+
+    logoJs.style.boxShadow = `10px 20px 20px ${colors[number++]}`
+
+    }else if( number.length <=  5){
+
+        number += shadowNumber
+
+        logoJs.style.boxShadow = `10px 20px 20px ${colors[number--]}`
 
 
 
-console.log('dziala')
+        
+    }
+    
 
 
 
-});
+
+
+
+}, 1000);
+console.log('działa')
+
+
+
+
+
+})
+
+
+
+
